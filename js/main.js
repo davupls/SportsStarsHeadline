4ccef1faf67d8f972e5a414fae87652391e030f2
David Mclean <davidmiltonmclean@gmail.com>
2020-09-11 19:29:17 -0500

    Update data to class function

--- js/main.js
+++ js/main.js
@@ -8,7 +8,6 @@ function post() {
     const dob = document.querySelector(".title__dob");
     const bio = document.querySelector(".title__content");
 
-
     headline.innerHTML = usain.headline;
     headlineSub.textContent = usain.subhead;
     img.src = usain.img;
@@ -18,3 +17,27 @@ function post() {
 }
 
 post();
+
+
+// let dropdown = document.querySelector('.dropdown')
+//     dropdown.addEventListener('click', (e) => {
+//         if (dropdown.classList.contains('closed')) {
+//         dropdown.classList.remove('closed')
+//     } else {
+//         dropdown.classList.add('closed')    
+//     }
+// })
+
+// let test = document.querySelectorAll(".menu__sel"),
+//     menu = document.querySelector(".menu");
+
+// menu.addEventListener("click", (e) => {
+//     let value = e.target.innerHTML;
+
+//     if (value === "USAIN BOLT") {
+//         post();
+//     } else {
+//         console.log(value)
+//     }
+// })
+
